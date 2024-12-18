2712e9e9489c612572ee9ca01f8b53000ab21dd3
Andrii Zadorozhnii <zador746@gmail.com>
2024-12-18 21:15:42 +0200

    Add search to phonebook

--- Lesson_12_Working_with_files/Homework_12_Phonebook_application/homework_11_working_with_files.py
+++ Lesson_12_Working_with_files/Homework_12_Phonebook_application/homework_11_working_with_files.py
@@ -1,20 +1,21 @@
 import json
 
-from Components.insert_user.add_user import add_user
-from Components.show_users import show_users
-from Components.update_user.update_users import update_user
+from Lesson_12_Working_with_files.Homework_12_Phonebook_application.Components.insert_user.add_user import add_user
+from Lesson_12_Working_with_files.Homework_12_Phonebook_application.Components.show_users import show_users
+from Lesson_12_Working_with_files.Homework_12_Phonebook_application.Components.update_user.update_users import \
+    update_user
 from Lesson_12_Working_with_files.Homework_12_Phonebook_application.Components.delete_user.delete_user import \
     delete_user
 from Lesson_12_Working_with_files.Homework_12_Phonebook_application.Components.load_users import load_user
 
-file_name = 'Components/insert_user/phones.json'
+file_name = 'Data_base/data_base.json'
 
 users = load_user(file_name)
 working = True
 while working:
 
     action = input(
-        'What do you want to do: Insert User(1)/Update User(2)/Delete User(3)/Search(4)/Show Users(5) or Exit?(e): ')
+        'What do you want to do: (1)Insert User/(2)Update User/(3)Delete User/(4)Search/(5)Show Users or (e)Exit?: ')
 
     if action == '1':
 
@@ -32,38 +33,106 @@ while working:
 
         def search_user(variable, filename):
             input_key = input(
-                'Please write what do you want to use for search |(1)Phone Number|(2)Name|(3)Family name|(4)City|(5)Country: ')
-            search_key = ''
-            counter = 0
+                'Please write what do you want to use for search |(1)Phone Number|(2)Name|(3)Family name|(4)Full Name|(5)City|(6)Country: ')
+
+            search_data = input('Please write what do you search: ')
 
             if input_key == '1':
-                search_key = 'name'
+
+                try:
+                    with open(filename, 'r+', encoding='utf-8') as file:
+                        variable = json.load(file)
+                        for user_number, details in variable.items():
+                            if user_number == search_data:
+                                a = []
+                                a.append(f'{user_number}{details}')
+                                print(' | '.join(a))  # need to make better looking
+                            else:
+                                print('No current phone number in data base')
+                except Exception:
+                    print('some Error')
+
             elif input_key == '2':
-                search_key = 'family name'
+
+                try:
+                    with open(filename, 'r+', encoding='utf-8') as file:
+                        variable = json.load(file)
+                        for user_number, details in variable.items():
+                            if details['name'].lower() == search_data.lower():
+                                a = []
+                                a.append(f'Phone number: {user_number:15}')
+                                for key, value in details.items():
+                                    a.append(f'{key:8}: {value:15}')
+
+                                print(' | '.join(a))
+                except Exception:
+                    print('Some Error')
+
             elif input_key == '3':
-                search_key = 'full name'
+
+                try:
+                    with open(filename, 'r+', encoding='utf-8') as file:
+                        variable = json.load(file)
+                        for user_number, details in variable.items():
+                            if details['family name'].lower() == search_data.lower():
+                                a = []
+                                a.append(f'Phone number: {user_number:15}')
+                                for key, value in details.items():
+                                    a.append(f'{key:8}: {value:15}')
+                                print(' | '.join(a))
+                except Exception:
+                    print('Some Error')
+
             elif input_key == '4':
-                search_key = 'city'
+                try:
+                    with open(filename, 'r+', encoding='utf-8') as file:
+                        variable = json.load(file)
+                        for user_number, details in variable.items():
+                            if (
+                            f'{details['name'].lower()}+" "+ {details['family name'].lower()}') == search_data.lower():
+                                a = []
+                                a.append(f'Phone number: {user_number:15}')
+                                for key, value in details.items():
+                                    a.append(f'{key:8}: {value:15}')
+
+                                print(' | '.join(a))
+                except Exception:
+                    print('Some Error')
             elif input_key == '5':
+                try:
+                    with open(filename, 'r+', encoding='utf-8') as file:
+                        variable = json.load(file)
+                        for user_number, details in variable.items():
+                            if details['user city'].lower() == search_data.lower():
+                                a = []
+                                a.append(f'Phone number: {user_number:15}')
+                                for key, value in details.items():
+                                    a.append(f'{key:8}: {value:15}')
+
+                                print(' | '.join(a))
+                except Exception:
+                    print('Some Error')
+            elif input_key == '6':
+
+                try:
+                    with open(filename, 'r+', encoding='utf-8') as file:
+                        variable = json.load(file)
+                        counter = 1
+                        for user_number, details in variable.items():
+                            if details['user country'].lower() == search_data.lower():
+                                a = []
+
+                                a.append(f'{counter}.Phone number: {user_number:15}')
+                                for key, value in details.items():
+                                    a.append(f'{key:8}: {value:15}')
+
+                                print(' | '.join(a))
+                        counter += 1
+                except Exception:
+                    print('Some Error')
                 search_key = 'country'
             else:
                 print('Wrong input')
-
-            print(search_key)
-
-            with open(filename, 'r+', encoding='utf-8') as file:
-                variable = json.load(file)
-
-            while counter == len(variable):
-
-                for user_number, details in variable.items():
-                    a = []
-                    for key, value in details.items():
-                        if key == search_key:
-                            a.append(f'{key.capitalize()}: {value:18}')
-                    print(" | ".join(a))
-
-                counter += 1
 
 
         search_user(users, file_name)
