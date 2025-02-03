051fd7f1043853afb8fe59a94e45d4dddacfbda3
Andrii Zadorozhnii <zador746@gmail.com>
2025-02-03 21:10:45 +0200

    Classwork_23_Stack/Queue/Deque_Classwork

---
+++ Lesson_23_Stack_Last_In_Last_Out/Lesson_23_Stack_Last_In_Last_Out_Classwork.py
@@ -0,0 +1,57 @@
+# list = [1, 2, 3, 4, 5, 6]
+# new_list = []
+# # while len(list) > 0:
+# #     new_list.append(list[-1])
+# #     list.pop()
+# #
+# # print(new_list)
+#
+# new_list_2 = list[::-1]
+# print(new_list_2)
+#
+# new_list_3 = []
+# for i in range(len(list)):
+#     new_list_3.append(list.pop())
+#
+# print(new_list_3)
+
+from queue import Queue
+from collections import deque
+
+import random
+
+random_list_1 = [random.randint(1, 999) for i in range(10)]
+random_list_2 = [random.randint(1, 999) for i in range(50)]
+random_list_3 = [random.randint(1, 999) for i in range(100)]
+
+
+class Stack():
+    def __init__(self):
+        self.list = []
+
+    def is_empty(self):
+        return len(self.list) == 0
+
+    def push(self, item):
+        self.list.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.list.pop()
+
+    def size(self):
+        return len(self.list)
+
+    def __str__(self):
+        result = '<Stack>\n'
+        for index, item in enumerate(self.list):
+            result += f'Index: {index}, Value: {str(item)}\n'
+        return result
+
+
+stack = Stack()
+
+for item in random_list_1:
+    stack.push(item)
+
+print(stack)
--- My/fun.py
+++ My/fun.py
@@ -1,68 +1,68 @@
-def binary_search(arr, target):
-    low = 0
-    high = len(arr) - 1
-    steps = 0
-
-    while low <= high:
-        steps += 1
-        mid = (low + high) // 2
-        if arr[mid] == target:
-            print(arr[mid])
-            return mid, steps
-        elif arr[mid] < target:
-            print(arr[mid])
-            low = mid + 1
-        else:
-            high = mid - 1
-
-    return -1, steps
-
-
-# Пример использования
-def array_list(number):
-    counter = 0
-    new_arr = []
-    while counter <= number:
-        new_arr.append(counter)
-        counter += 1
-    return new_arr
-
-
-arr = array_list(9999)
-# print(arr)
-
-target = 9999
-
-result, steps = binary_search(arr, target)
-
-if result != -1:
-    print(
-        f"Элемент найден на индексе {result}. Количество шагов: {steps}"
-    )
-else:
-    print(
-        f"Элемент не найден. Количество шагов: {steps}"
-    )
-
-
-def bubble_sort(arr):
-    n = len(arr)
-    steps = 0
-    for i in range(n):
-        swapped = False
-        for j in range(0, n - i - 1):
-            steps += 1
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                swapped = True
-        if not swapped:
-            break
-    return arr, steps
-
-
-# Пример использования
-arr = [64, 34, 25, 12, 22, 11, 90]
-sorted_arr, steps = bubble_sort(arr)
-
-print(f"Отсортированный массив: {sorted_arr}")
-print(f"Количество шагов: {steps}")
+# def binary_search(arr, target):
+#     low = 0
+#     high = len(arr) - 1
+#     steps = 0
+#
+#     while low <= high:
+#         steps += 1
+#         mid = (low + high) // 2
+#         if arr[mid] == target:
+#             print(arr[mid])
+#             return mid, steps
+#         elif arr[mid] < target:
+#             print(arr[mid])
+#             low = mid + 1
+#         else:
+#             high = mid - 1
+#
+#     return -1, steps
+#
+#
+# # Пример использования
+# def array_list(number):
+#     counter = 0
+#     new_arr = []
+#     while counter <= number:
+#         new_arr.append(counter)
+#         counter += 1
+#     return new_arr
+#
+#
+# arr = array_list(9999)
+# # print(arr)
+#
+# target = 9999
+#
+# result, steps = binary_search(arr, target)
+#
+# if result != -1:
+#     print(
+#         f"Элемент найден на индексе {result}. Количество шагов: {steps}"
+#     )
+# else:
+#     print(
+#         f"Элемент не найден. Количество шагов: {steps}"
+#     )
+#
+#
+# def bubble_sort(arr):
+#     n = len(arr)
+#     steps = 0
+#     for i in range(n):
+#         swapped = False
+#         for j in range(0, n - i - 1):
+#             steps += 1
+#             if arr[j] > arr[j + 1]:
+#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+#                 swapped = True
+#         if not swapped:
+#             break
+#     return arr, steps
+#
+#
+# # Пример использования
+# arr = [64, 34, 25, 12, 22, 11, 90]
+# sorted_arr, steps = bubble_sort(arr)
+#
+# print(f"Отсортированный массив: {sorted_arr}")
+# print(f"Количество шагов: {steps}")
